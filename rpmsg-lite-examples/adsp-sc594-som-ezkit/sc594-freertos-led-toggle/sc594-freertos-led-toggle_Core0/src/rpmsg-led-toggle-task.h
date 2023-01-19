5a16347c3f0aebfbfd987bfdb6865f876b1b05f9
Henrik Mau <Henrik.Mau@analog.com>
2023-01-19 11:27:37 +0000

    RPMsg-lite: Added FreeRTOS LED toggle example for SC594.  Added downloaded and autogenerated code for FREERTOS to .gitignore as the plugin will download a copy for each project and core it is included in

---
+++ rpmsg-lite-examples/adsp-sc594-som-ezkit/sc594-freertos-led-toggle/sc594-freertos-led-toggle_Core0/src/rpmsg-led-toggle-task.h
@@ -0,0 +1,27 @@
+/*
+ **
+ ** Source file generated on January 10, 2023 at 08:57:18.
+ **
+ ** Copyright (C) 2023 Analog Devices Inc., All Rights Reserved.
+ **
+ ** This file is generated when the FreeRTOS Add-In is added to a CrossCore Embedded Studio project,
+ ** or if this file has been removed and the project is re-opened.
+ **
+ ** It is safe to edit this file.
+ */
+
+
+#ifndef FREERTOS_USER_APPLICATION_H_
+#define FREERTOS_USER_APPLICATION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void rpmsgLedToggleTask( void* );
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* FREERTOS_USER_APPLICATION_H_ */
+
